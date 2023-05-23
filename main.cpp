58067a82d25206c3dfa754c2e71bea68fe7f2929
HomePC <HomePC@HOME-PC>
2023-05-23 16:27:34 +0300

    just new commit

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 static const std::string base64_alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 std::string StringToBase64(const std::string& source_string) {
-	//ѕреобразование исходной строки в битовую последовательность
+	//Converting a source string to a bit sequence
 	std::string bit_string;
 	std::bitset<8> binary_char;
 	for (const char& c : source_string) {
 		binary_char = std::bitset<8>(c);
 		bit_string += binary_char.to_string();
 	}
-	//Ќахождение остатка от делени€, последующее добавление нулевых бит дл€ выравнивани€
+	//Finding the remainder of the division, then adding zero bits for alignment
 	size_t remainder = bit_string.size() % 24;
 	if (remainder == 8) {
 		bit_string.append(4, '0');
@@ -21,14 +21,14 @@ std::string StringToBase64(const std::string& source_string) {
 	else if (remainder == 16) {
 		bit_string.append(2, '0');
 	}
-	//ѕреобразование битовой последовательности в base64
+	//Converting a bit sequence to base64
 	std::string base64_string;
 	std::bitset<6> binary_number;
 	for (size_t i = 0; i < bit_string.size(); i += 6) {
 		binary_number = std::bitset<6>(bit_string.substr(i, 6));
 		base64_string += base64_alphabet[binary_number.to_ulong()];
 	}
-	//«аполнение символами "=", если исходные данные не дел€тс€ нацело на 3 байта
+	//Filling with "=" characters if the source data is not completely divided into 3 bytes
 	if (remainder == 8) {
 		base64_string.append(2, '=');
 	}
@@ -40,23 +40,23 @@ std::string StringToBase64(const std::string& source_string) {
 }
 
 std::string Base64ToString(const std::string& base64_string) {
-	//Ќахождение количества вхождений символа '=' дл€ его пропуска
+	//Finding the number of occurrences of the '=' character to skip it
 	int counter = std::count(base64_string.begin(), base64_string.end(), '=');
-	//ѕреобразование строки base64 в битовую последовательность
+	//Converting a base64 string to a bit sequence
 	std::string bit_string;
 	std::bitset<6> binary_number;
 	for (size_t i = 0; i < base64_string.size() - counter; i++) {
 		binary_number = std::bitset<6>(base64_alphabet.find(base64_string[i]));
 		bit_string += binary_number.to_string();
 	}
-	//”даление лишних нулевых бит
+	//Removing extra zero bits
 	if (counter == 2) {
 		bit_string.resize(bit_string.size() - 4);
 	}
 	else if (counter == 1) {
 		bit_string.resize(bit_string.size() - 2);
 	}
-	//ѕреобразование битовой последовательности в обычную строку
+	//Converting a bit sequence to a regular string
 	std::string decoded_string;
 	std::bitset<8> binary_char;
 	for (size_t i = 0; i < bit_string.size(); i += 8) {
